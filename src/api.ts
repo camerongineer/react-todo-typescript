ced6fc37010e5430e1ce285b3ecd4a3622dafe0b
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-24 11:05:01 -0800

    Remove backend server

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const fetchItems = () => axios.get("/api/items");
+const fetchItems = () => axios.get(`${process.env.REACT_APP_API_BASE_URL}/items`);
 
 export {
     fetchItems
